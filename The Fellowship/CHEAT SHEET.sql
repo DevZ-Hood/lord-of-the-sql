fe6be0438512a058af13524177900c322d80b527
MagaldiLucas <98914208+MagaldiLucas@users.noreply.github.com>
2023-09-27 16:42:31 -0300

    Lord of sql (#2)
    * activities
    
    * elaborate flow
    
    * thurin scene
    
    * thurin scene
    
    * bilbos birthday
    
    * last ammends
    
    ---------
    
    Co-authored-by: Machado <chmacha@SA.xom.com>

---
+++ The Fellowship/CHEAT SHEET.sql
@@ -0,0 +1,35 @@
+------------------ INSERT ---------------------
+
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+
+
+
+
+------------------ SELECT ---------------------
+SELECT column1, column2, ...
+FROM table_name;
+
+
+
+
+------------------ DELETE ---------------------
+DELETE FROM table_name WHERE condition;
+
+
+
+
+------------------ UPDATE ---------------------
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
+
+
+
+--------------- Create Table ------------------
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
